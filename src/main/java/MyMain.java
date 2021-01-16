79c4af568008225c972627fc81b2b1a62fe5fe32
hQian2718 <22kqian@athenian.org>
2021-01-16 10:41:39 +0000

    Wrote methods for finding mean, median and mode in sorted 2d arrays

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,21 +2,57 @@ public class MyMain {
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        double result = 0.0;
+        int size = 0;
+
+        for(double[] arr : mat){
+            for(double a : arr){
+                result += a;
+                size ++;
+            }
+        }
+
+        return result / size;
     }
 
     // Returns the median of the 2D array mat
     public static double median(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+
+        if(mat.length == 0 || mat[0].length == 0)   return 0.0;
+        int size = mat.length * mat[0].length;
+        double median = 0;
+
+        int[] idx = {(size - 1) / 2, size / 2};
+
+        median += mat[idx[0]/mat[0].length][idx[0] % mat[0].length];
+        median += mat[idx[1]/mat[0].length][idx[1] % mat[0].length];
+        median /= 2;
+        return median;
     }
     
 
     // Returns the mode of the 2D array mat
     public static double mode(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        double mode = mat[0][0];
+        double pre = mat[0][0];
+        int maxNum = 0;
+        int num = 0;
+
+        for(double[] arr:mat){
+            for(double d:arr){
+                if(d == pre){
+                    num ++;
+                }else{
+                    if(num > maxNum){
+                        maxNum = num;
+                        mode = pre;
+                    }
+                    num = 1;
+                    pre = d;
+                }
+            }
+        }
+        return mode;
     }
 
 
